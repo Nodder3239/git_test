8319392ac95db0cee2a92d5ec16eca43b179d52d
nodder3239 <show3239@naver.com>
2023-09-22 18:16:37 +0900

    javaproject

--- ch09/src/streams/Student.java
+++ ch09/src/streams/Student.java
@@ -1,28 +1,19 @@
 package streams;
 
 public class Student {
-	private String name;
-	private int score;
+	private String name;  //이름
+	private int score;    //점수
 	
 	public Student(String name, int score) {
 		this.name = name;
 		this.score = score;
-	
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public int getScore() {
 		return score;
-	}
-
-	public void setScore(int score) {
-		this.score = score;
 	}
 }
