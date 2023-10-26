b1be877a45a406956c04fb92715089ca40aa2d6d
nodder3239 <show3239@naver.com>
2023-10-26 17:51:08 +0900

    javaworks

--- dbconnection/src/dbcrud/Practice.java
+++ dbconnection/src/dbcrud/Practice.java
@@ -1,10 +1,48 @@
 package dbcrud;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.User;
+
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/xe", "c##mydb", "pwmydb");
+			System.out.println("연결 성공!");
+			
+			String sql = "SELECT * FROM users";
+			pstmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = pstmt.executeQuery();
+			List<User> userList = new ArrayList<>();
+			while(rs.next()) {
+				User user = new User();
+				user.setUserId(rs.getString("userid"));
+				user.setUserName(rs.getString("username"));
+				user.setUserPassword(rs.getString("userpassword"));
+				user.setUserAge(rs.getInt("userage"));
+				user.setUserEmail(rs.getString("useremail"));
+				
+				userList.add(user);
+				
+				
+			}
+			for(User user : userList) {
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
